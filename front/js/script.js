99cefe3b800b58aff12d4d32c41919e9e3d8d1ca
Miilenna <milena200.fv@gmail.com>
2024-10-14 16:09:29 +0200

    js modificat fins el primer apartat

--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Cridem a l'endpoint de l'API fent un fetch
-    ______________________________________________
+    fetch("http://localhost:8000/alumne/list")
         .then(response => {
             if (!response.ok) {
                 throw new Error("Error a la resposta del servidor");
@@ -15,13 +15,32 @@ document.addEventListener("DOMContentLoaded", function() {
             data.forEach(alumne => {
                 const row = document.createElement("tr");
 
+                //Nom de l'alumne
                 const nomAluCell = document.createElement("td");
                 nomAluCell.textContent = alumne.NomAlumne;
                 row.appendChild(nomAluCell);
 
                 // Repetir per tots els altres camps restants que retorna l'endpoint
-                _____________________________________________
+                
+                //Cicle
+                const CicleCell = document.createElement("td");
+                CicleCell.textContent = alumne.Cicle;
+                row.appendChild(CicleCell);
 
+                //Curs
+                const CursCell = document.createElement("td");
+                CursCell.textContent = alumne.Curs;
+                row.appendChild(CursCell);
+
+                //Grup
+                const GrupCell = document.createElement("td");
+                GrupCell.textContent = alumne.Grup;
+                row.appendChild(GrupCell);
+
+                //Nom de l'aula
+                const DescAulaCell = document.createElement("td");
+                DescAulaCell.textContent = alumne.DescAula;
+                row.appendChild(DescAulaCell);
                 
 
                 alumnesTableBody.appendChild(row);
